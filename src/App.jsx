0471baab3bc1cd241c38e13e88c3dccc096534f2
np03s190038 <thapa.sumit007@gmail.com>
2024-10-02 12:06:29 +0545

    Initial Home page Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-export default function App(){
-  return(
-    <h1>Hello World</h1>
+import Home from "./assets/pages/Home"
+import About from "./assets/pages/About"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
-
-function About(){
-  return(
-    <h2>About Page</h2>
-  )
-}
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
-)
+    <App />
+  </StrictMode>
+);
