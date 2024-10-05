ecdd19f49aa0e510d365fac8835e59f2ae4038ed
np03s190038 <thapa.sumit007@gmail.com>
2024-10-05 16:03:35 +0545

    Nexted Route Implementation
    Added nested routes to the Vans subpages which shares some similar UIs

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
-import Home from "./assets/pages/Home"
-import About from "./assets/pages/About"
-import Vans from "./assets/pages/Vans"
-import VanDetail from "./assets/pages/Vandetail"
+import Home from "./assets/pages/vans/Home"
+import About from "./assets/pages/vans/About"
+import Vans from "./assets/pages/vans/Vans"
+import VanDetail from "./assets/pages/vans/VanDetail"
+import Dashboard from "./assets/host/Dashboard"
+import Income from "./assets/host/Income"
+import Reviews from "./assets/host/Reviews"
+
+import Layout from "./assets/components/Layout"
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import "./server"
 
 export default function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/host" element={<Dashboard />} />
+          <Route path="/host/income" element={<Income />} />
+          <Route path="/host/reviews" element={<Reviews />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
